b481f68ebf599eeb750f06d70f1ec1ccecc3e7d3
Daniel truong <dtruong@uvic.ca>
2018-03-12 12:37:11 -0700

    added reorg function

--- run.py
+++ run.py
@@ -66,7 +66,7 @@ class MyNetwork(object):
                 tf.assign(self.n_mean, self.n_mean_in),
                 tf.assign(self.n_range, self.n_range_in),
             )
-
+    
     def _build_model(self):
         """
 
@@ -75,6 +75,16 @@ class MyNetwork(object):
             
         """
 
+        def reorg(net, stride=2, name='reorg'):
+            batch_size, height, width, channels = net.get_shape().as_list()
+            _height, _width, _channel = height // stride, width // stride, channels * stride * stride
+            with tf.name_scope(name) as name:
+                net = tf.reshape(net, [batch_size, _height, stride, _width, stride, channels])
+                net = tf.transpose(net, [0, 1, 3, 2, 4, 5]) # batch_size, _height, _width, stride, stride, channels
+                net = tf.reshape(net, [batch_size, _height, _width, -1], name=name)
+        return net
+
+        
         def leaky_relu(inputs, alpha=.1):
             with tf.name_scope('leaky_relu') as name:
                 data = tf.identity(inputs, name='data')
